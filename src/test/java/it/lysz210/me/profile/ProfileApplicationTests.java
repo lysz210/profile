2aeacbee095972912a59bd781941ae45e3f5d40a
Lingyong Sun <lingyong.sun@gmail.com>
2022-07-03 11:31:10 +0200

    init project

---
+++ src/test/java/it/lysz210/me/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lysz210.me.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
