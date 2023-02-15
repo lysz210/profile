43e33e4b239b4e07623e0fdf0434d6443aac99e4
Lingyong Sun <lingyong.sun@gmail.com>
2023-02-15 22:20:16 +0100

    Further optimize parallel execution on init DynamoDB

---
+++ src/main/java/it/lysz210/profile/lib/common/I18nazed.java
@@ -0,0 +1,8 @@
+package it.lysz210.profile.lib.common;
+
+import java.util.Locale;
+
+public interface I18nazed {
+    Locale getLocale();
+    void setLocale(Locale locale);
+}
--- src/main/java/it/lysz210/profile/lib/dynamodb/repository/WithTable.java
+++ src/main/java/it/lysz210/profile/lib/dynamodb/repository/WithTable.java
@@ -3,5 +3,7 @@ package it.lysz210.profile.lib.dynamodb.repository;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbAsyncTable;
 
 public interface WithTable<T> {
-    public DynamoDbAsyncTable<T> getTable();
+
+    Class<T> getEntityType();
+    DynamoDbAsyncTable<T> getTable();
 }
