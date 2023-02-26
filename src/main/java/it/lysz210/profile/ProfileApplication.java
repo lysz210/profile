9a49d5556fb7fffb051352139ea32b714e626d60
Lingyong Sun <lingyong.sun@gmail.com>
2023-02-26 10:42:53 +0100

    update debug with reactor-tools

--- src/main/java/it/lysz210/profile/ProfileApplication.java
+++ src/main/java/it/lysz210/profile/ProfileApplication.java
@@ -2,11 +2,13 @@ package it.lysz210.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 public class ProfileApplication {
 
 	public static void main(String[] args) {
+		ReactorDebugAgent.init();
 		SpringApplication.run(ProfileApplication.class, args);
 	}
 
