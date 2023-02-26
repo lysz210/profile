620a6d06e2cbfa565519ce8bf7252444d957a434
Lingyong Sun <lingyong.sun@gmail.com>
2023-02-26 13:03:08 +0100

    update spring boot starter to enable context-propagation

--- src/main/java/it/lysz210/profile/ProfileApplication.java
+++ src/main/java/it/lysz210/profile/ProfileApplication.java
@@ -2,6 +2,7 @@ package it.lysz210.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
@@ -9,6 +10,7 @@ public class ProfileApplication {
 
 	public static void main(String[] args) {
 		ReactorDebugAgent.init();
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(ProfileApplication.class, args);
 	}
 
