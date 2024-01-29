e55652f8eb7950860809ea57a6dac0cb6b4bbb61
Lingyong Sun <lingyong.sun@gmail.com>
2024-01-29 19:45:33 +0100

    restart project as pulumi

--- src/main/java/it/lysz210/profile/lib/common/I18nazed.java
+++
@@ -1,8 +0,0 @@
-package it.lysz210.profile.lib.common;
-
-import java.util.Locale;
-
-public interface I18nazed {
-    Locale getLocale();
-    void setLocale(Locale locale);
-}
--- src/main/java/it/lysz210/profile/me/personaldetails/PersonalDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package it.lysz210.profile.me.personaldetails;
-
-import it.lysz210.profile.lib.dynamodb.repository.query.FindAll;
-
-public interface PersonalDetailsRepository
-    extends FindAll<PersonalDetails>
-{
-}
