96b5a5d32e328ba3c5f2ac33aeafee012144d943
Lingyong Sun <lingyong.sun@gmail.com>
2024-02-03 21:52:23 +0100

    remove set feature from ArrayNodeSerializer
    DynamoDB sets don't maintain input order but always sort elements

--- app/src/main/java/it/lysz210/profile/serializers/ArrayNodeSerializer.java
+++ app/src/main/java/it/lysz210/profile/serializers/ArrayNodeSerializer.java
@@ -4,14 +4,10 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ValueNode;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import org.springframework.lang.NonNull;
 
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.Objects;
-import java.util.Optional;
 
 public class ArrayNodeSerializer extends StdSerializer<ArrayNode> {
 
@@ -19,43 +15,16 @@ public class ArrayNodeSerializer extends StdSerializer<ArrayNode> {
         super(type);
     }
 
-    Optional<ValueNodeSerializer.DynamoDbSimpleType> retrieveCandicateSetTypeFor (@NonNull ArrayNode arrayNode) {
-        if (arrayNode.isEmpty()) {
-            return Optional.empty();
-        }
-        ValueNodeSerializer.DynamoDbSimpleType candidate = null;
-        for (Iterator<JsonNode> it = arrayNode.elements(); it.hasNext(); ) {
-            final var node = it.next();
-            if (node.isNull() || !node.isValueNode()) {
-                return Optional.empty();
-            }
-            final var dynamoType = ValueNodeSerializer.DynamoDbSimpleType.forValueNodeInstance((ValueNode) node);
-            if (Objects.isNull(dynamoType) || (Objects.nonNull(candidate) && candidate != dynamoType )) {
-                return Optional.empty();
-            }
-            candidate = dynamoType;
-        }
-        return Optional.ofNullable(candidate);
-    }
-
     @Override
     public void serialize(ArrayNode arrayNode, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-        final var candidateSetType = retrieveCandicateSetTypeFor(arrayNode);
 
         jsonGenerator.writeStartObject();
-        jsonGenerator.writeFieldName(
-                candidateSetType.map(type -> type + "S").orElse("L")
-        );
+        jsonGenerator.writeFieldName("L");
 
         jsonGenerator.writeStartArray();
 
         for (Iterator<JsonNode> it = arrayNode.elements(); it.hasNext(); ) {
-            final var node = it.next();
-            if (candidateSetType.isEmpty()) {
-                serializerProvider.defaultSerializeValue(node, jsonGenerator);
-            } else {
-                jsonGenerator.writeString(node.asText());
-            }
+            serializerProvider.defaultSerializeValue(it.next(), jsonGenerator);
         }
 
         jsonGenerator.writeEndArray();
