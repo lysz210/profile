6cd8aabc81bf256d1f4ed074813ce83cc53f8ba8
Lingyong Sun <lingyong.sun@gmail.com>
2024-03-05 21:27:23 +0100

    remove java project

--- app/src/main/java/it/lysz210/profile/repositories/I18nRepository.java
+++
@@ -1,9 +0,0 @@
-package it.lysz210.profile.repositories;
-
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import java.nio.file.Path;
-
-public interface I18nRepository {
-    ObjectNode retrieveTranslation(Path source);
-}
